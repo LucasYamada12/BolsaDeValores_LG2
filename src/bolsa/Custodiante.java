3cd292c8cd1d46396b64b4b591c009f1e9b97ea9
LucasYamada12 <128192347+LucasYamada12@users.noreply.github.com>
2023-10-21 15:56:59 -0300

    Update Custodiante.java

--- src/bolsa/Custodiante.java
+++ src/bolsa/Custodiante.java
@@ -1,1 +1,24 @@
+package bolsa;
+import estruturadados.DynamicArray;
 
+public class Custodiante {
+    private String nome;
+    private DynamicArray<Carteira> carteiras;
+
+    public Custodiante(String nome) {
+        this.nome = nome;
+        this.carteiras = carteiras;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public DynamicArray<Carteira> getCarteiras() {
+        return carteiras;
+    }
+
+    public void setCarteiras(DynamicArray<Carteira> carteiras) {
+        this.carteiras = carteiras;
+    }
+}
