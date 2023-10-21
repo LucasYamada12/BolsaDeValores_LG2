6e39ce68c2b2a7759d64ddcf3182ecc6b24cdd12
LucasYamada12 <128192347+LucasYamada12@users.noreply.github.com>
2023-10-21 15:53:40 -0300

    Update Acao.java

--- src/bolsa/Acao.java
+++ src/bolsa/Acao.java
@@ -5,4 +5,8 @@ public class Acao extends Ativo{
     public Acao(String nome, String codigo, double cotacao, int lotePadrao, double dividendos) {
         super(nome, codigo, cotacao, lotePadrao, dividendos);
     }
+
+    public Acao(String nome, String codigo, double cotacao, int lotePadrao) {
+        super(nome, codigo, cotacao, lotePadrao, 0);
+    }
 }
